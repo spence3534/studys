fdc6c6e65fba2fedb9e47845e4ee6251fc79d475
a820792395 <8207923@qq.com>
2020-07-06 01:01:29 +0800

    第四章完结，添加第七章

--- ts/04.js
+++ ts/04.js
@@ -47,14 +47,14 @@
 function searchXiaoJieJie() {
     var xuqiu = [];
     for (var _i = 0; _i < arguments.length; _i++) {
-        xuqiu[_i] = arguments[_i];
+      xuqiu[_i] = arguments[_i];
     }
     var yy = "找到了";
     for (var i = 0; i < xuqiu.length; i++) {
-        yy = yy + xuqiu[i];
-        if (i < xuqiu.length) {
-          yy = yy + "、";
-        }
+      yy = yy + xuqiu[i];
+      if (i < xuqiu.length) {
+        yy = yy + "、";
+      }
     }
     yy = yy + "的小姐姐";
     return yy;
