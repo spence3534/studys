274f2112baf9a34ab2c93f18f431c30f53a3ae86
undefined <undefined@undefineddeMacBook-Pro.local>
2020-12-06 14:28:58 +0800

    文件整理

--- ts/04.js
+++ ts/04.js
@@ -45,19 +45,19 @@
   是一个数组，传递几个实参过来都可以直接存在形参的数组中。
 */
 function searchXiaoJieJie() {
-    var xuqiu = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-      xuqiu[_i] = arguments[_i];
+  var xuqiu = [];
+  for (var _i = 0; _i < arguments.length; _i++) {
+    xuqiu[_i] = arguments[_i];
+  }
+  var yy = "找到了";
+  for (var i = 0; i < xuqiu.length; i++) {
+    yy = yy + xuqiu[i];
+    if (i < xuqiu.length) {
+      yy = yy + "、";
     }
-    var yy = "找到了";
-    for (var i = 0; i < xuqiu.length; i++) {
-      yy = yy + xuqiu[i];
-      if (i < xuqiu.length) {
-        yy = yy + "、";
-      }
-    }
-    yy = yy + "的小姐姐";
-    return yy;
+  }
+  yy = yy + "的小姐姐";
+  return yy;
 }
 var result = searchXiaoJieJie("22岁", "大长腿", "瓜子脸", "水蛇腰");
 console.log(result);
