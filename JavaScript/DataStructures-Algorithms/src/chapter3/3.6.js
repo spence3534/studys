c1c3d092ddfcb6aceda2c4ceb2043ec1c7716073
a820792395 <820792395@qq.com>
2021-01-15 21:21:35 +0800

    修改第三章内容

--- JavaScript/DataStructures-Algorithms/src/chapter3/3.6.js
+++ JavaScript/DataStructures-Algorithms/src/chapter3/3.6.js
@@ -53,18 +53,3 @@
   printMatrix(averageTemp);
   ```
 */
-let averageTemp = [];
-averageTemp[0] = [72, 75, 79, 79, 81, 81];
-averageTemp[1] = [81, 79, 75, 75, 73, 73];
-
-function printMatrix(arr) {
-  console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-    for (let j = 0; j < arr[i].length; j++) {
-      console.log(arr[i][j]);
-    }
-  }
-}
-
-printMatrix(averageTemp);
