59633c5d1827c6f194d42e3a4092469a1e944841
820792395@qq.com <820792395@qq.com>
2021-02-03 18:24:46 +0800

    添加第六章第二节

--- JavaScript/DataStructures-Algorithms/src/chapter6/6.2.js
+++ JavaScript/DataStructures-Algorithms/src/chapter6/6.2.js
@@ -1,2 +1,154 @@
 /*
+  ## 双向链表
+
+  双向链表和普通链表的区别在于，链表只是单向的链接。而在双向链表里，链接是双向的：第一个元素链向第二个元素，第二个元素又链向第一个元素，
+  如下图。
+
+  <img src="./images/6/6-2-1.png" />
+
+
  */
+
+class LinkedList {
+  constructor() {
+    this.count = 0; // 存储链表元素数量
+    this.head = undefined; // 第一个元素的引用
+    // 比较链表中的元素是否相等
+    this.equalsFn = function (a, b) {
+      return a === b;
+    };
+  }
+
+  push(ele) {
+    const node = new Node(ele);
+    let current = "";
+    if (this.head === undefined) {
+      // node里面的next属性始终undefined
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next !== undefined) {
+        // 设置current为下一个元素
+        current = current.next;
+      }
+      // current.next为undefined就表示到了链表的尾部，然后把最后一个元素的next属性设置为下一元素
+      current.next = node;
+    }
+    this.count++;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const prev = this.getElementAt(index - 1);
+        console.log(prev);
+        current = prev.next;
+        prev.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      // 初始化node变量，从链表的第一个元素开始迭代
+      let node = this.head;
+      for (let i = 0; i < index && node !== undefined; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  insert(ele, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(ele);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        // 需要添加新节点位置的前一个位置
+        const prev = this.getElementAt(index - 1);
+        // 需要添加新节点位置的后一个位置
+        const current = prev.next;
+        node.next = current;
+        prev.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  indexOf(ele) {
+    let current = this.head;
+    for (let i = 0; i < this.count && current !== undefined; i++) {
+      if (this.equalsFn(ele, current.element)) {
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+
+  remove(ele) {
+    const index = this.indexOf(ele);
+    return this.removeAt(index);
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  toString() {
+    if (this.head === undefined) {
+      return "";
+    }
+
+    let objString = this.head.element;
+    let current = this.head.next;
+    for (let i = 0; i < this.size() && current !== undefined; i++) {
+      objString = `${objString}, ${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+}
+// Node类：想要添加到链表中的项。
+class Node {
+  constructor(ele) {
+    this.element = ele; // 想要加入链表元素的值
+    this.next = undefined; // 指向链表中下一个元素的指针。
+  }
+}
+
+class DoublyNode extends Node {
+  constructor(ele, next, prev) {
+    super(ele, next);
+    this.prev = prev; // 新增：前一个元素
+    console.log(prev);
+  }
+}
+console.log(new DoublyNode());
+class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn) {
+    super(equalsFn);
+    this.tail = undefined; // 新增 最后一个元素的引用
+  }
+}
