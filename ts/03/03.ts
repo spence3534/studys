971e761e160cab91d91ba2dcabd96ca530838058
a820792395 <820792395@qq.com>
2022-01-05 22:24:21 +0800

    ts添加第三章内容

--- ts/03/03.ts
+++ ts/03/03.ts
@@ -1,1 +1,9 @@
 
+interface Person {
+  name: string
+}
+// Subsequent property declarations must have the same type.  Property 'name' must be of type 'string', but here has type 'number'
+
+class Cat implements Person {
+  name: string = '图图'
+}
