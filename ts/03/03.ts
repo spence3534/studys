213b9a1b4b5af9f5a57ea27ddca9d84a16e5f945
a820792395 <820792395@qq.com>
2022-01-09 22:47:02 +0800

    feat: 添加ts第三章内容

--- ts/03/03.ts
+++ ts/03/03.ts
@@ -1,9 +1,33 @@
+type ClassConstructor<T> = new (...args: any[]) => T
 
-interface Person {
-  name: string
-}
-// Subsequent property declarations must have the same type.  Property 'name' must be of type 'string', but here has type 'number'
+function withEZDebug<C extends ClassConstructor<{ getDebugValue(): object }>>(Class: C) {
+  return class extends Class {
 
-class Cat implements Person {
-  name: string = '图图'
+    debug() {
+      console.log(Class.constructor)
+      let Name = Class.constructor
+      let value = this.getDebugValue()
+      return Name + `( ${JSON.stringify(value)} )`
+    }
+  }
 }
+
+
+class HardToDebugUser {
+  constructor(
+    private id: number,
+    private firstName: string,
+    private lastName: string
+  ) { }
+
+  getDebugValue() {
+    return {
+      id: this.id,
+      name: `${this.firstName}  ${this.lastName}`
+    }
+  }
+}
+
+let User = withEZDebug(HardToDebugUser)
+let user = new User(3, 'emma', 'gluzman')
+console.log(user.debug())
